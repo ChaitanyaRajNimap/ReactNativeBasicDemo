dff83d292496b278690a6bbcb22d62b9fa216814
ChaitanyaRajNimap <chaitanyarajiwade@nimapinfotech.com>
2022-12-16 13:48:39 +0530

    Adding golas functionality done

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(newText) {
+    setEnteredGoal(newText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals([...courseGoals, enteredGoal]);
+  }
+
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course goal!" />
-        <Button title="Add Goal" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          // onChange={goalInputHandler}
+          onChangeText={(newText) => goalInputHandler(newText)}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
@@ -16,6 +35,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "70%",
+    marginRight: 10,
+    padding: 5,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
